117470d5e23aa3dde2315fe7fce6c77cd0da69a7
robertojunior92 <robertolourenco92@gmail.com>
2015-11-23 08:20:37 -0200

    Botões
    Config Botões

--- dados/itens.php
+++ dados/itens.php
@@ -4,7 +4,7 @@
 
 		include($raiz."include/conexaoMySQL.php");
 
-      $query = "select * from itens_telefone";
+    /*  $query = "select * from itens_telefone";
       $consulta = mysqli_query($conexao, $query) or die (mysqli_error());
 
       $dados = array();
@@ -24,7 +24,56 @@
       $saida = array();
       $saida['data'] = $dados;
 
-      $saida['total'] = $cont++;
+      $saida['total'] = $cont++;*/
+
+      if (isset($GET["create"])){ //VERIFICA SE O USUARIO CLICOU NO BOTÃO ADICIONAR
+
+        $rows = $_SESSION ['itens'];
+        $rows[$_GET ['id']] =
+        array(
+          'descricao'=> $_GET['Descricao'],
+          'quantidade'=> $_GET['Quantidade'],
+          'serial_id' => $_GET['ID']
+          );
+        $_SESSION['itens'] = $rows;
+      }
+      elseif (isset($_GET['save'])){
+        $row = $_SESSION['itens'];
+
+        unset($rows[trim($_GET['id'])]);
+
+        $rows[$_GET['id']] = 
+        array(
+          'descricao'=>$_GET['Descricao'],
+          'quantidade'=>$_GET['Quantidade'],
+          'serial_id'=>$_GET['ID']
+          );
+        $_SESSION['itens'] = $rows;
+      }
+      elseif(isset($_GET['cancel'])){
+        $rows = $_SESSION['itens'];
+        unset($rows[trim($_GET['cancel'])]);
+        $_SESSION['itens'] = $rows;
+      }
+      else{
+        $query = "select * from itens_telefone";
+        $rs = mysqli_query($conexao, $query) or die (mysqli_error());
+
+        $dados = array();
+
+        while($res = mysql_fetch_array($rs)) {
+          $rows[$res["id"]] = array("descricao" => $res['descricao'],"quantidade" => $res['quantidade'], "serial_id" => $res['serial_id']);
+        }
+
+/*
+        if(isset($_SESSION['itens'])){
+          $rows = $_SESSION['itens'];
+        }
+*/
+      }
+
+
+
 
       // Fechar a conexão com o Banco
       mysqli_close($conexao);
